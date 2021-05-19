df6f38899d9ceb511a292a87c09fe930c0fa984a
Bryan20gill <Bryan20gill@gmail.com>
2021-05-19 04:49:44 +0100

    added index.php and composer.js

--- assets/api.js
+++ assets/api.js
@@ -1,31 +1,26 @@
 function fetchInfo () {
-    window.fetch('https://restcountries.eu/rest/v2/all') 
+    fetch('https://mawaqif.herokuapp.com/api/checks') 
       .then(response => response.json())
       .then(jsonObj => displayUi(jsonObj))
-      .catch(() => window.alert('API Could not be reached at this time'))
+      .catch(() => window.alert('Checks API Could not be reached at this time'))
 }
 
 // This displays the data on the page
-function displayUi (country) {
-    const { name, capital, languages, currencies, population, region } = country[Math.floor(Math.random() * 150)]
+function displayUi (check) {
+    const { plate_number, packing_space_id, is_empty } = check[1]
     const template = `
-    <div>
-    <h1 id="head">${name}</h1>
-    <p id="content"> This is a country with its capital in ${capital}. 
-    The language(s) spoken here are ${languages[0].name}. The nation of ${name} is 
-    located in the ${region} region with a population of ${population} and uses ${currencies[0].name} as it's currency</P>
-    </div>
+    
 
 
     <div class="col-lg-4 g-mb-30">
         <article class="g-pa-25 u-shadow-v11 rounded">
             <h2 class="h4 g-mb-15">
-                <a class="u-link-v5 g-color-gray-dark-v1 g-color-primary--hover" href="#">Parking Space 1</a>
+                <a class="u-link-v5 g-color-gray-dark-v1 g-color-primary--hover" href="#">Parking Space ${packing_space_id}</a>
             </h2>
 
             <ul class="list-inline d-flex justify-content-between g-mb-20">
             <li class="list-inline-item">
-                <i class="icon-eye g-pos-rel g-top-1 g-color-gray-dark-v5 g-mr-5"></i> 722AQ53GY8
+                <i class="icon-eye g-pos-rel g-top-1 g-color-gray-dark-v5 g-mr-5"></i> ${plate_number}
             </li>
             <li class="list-inline-item">
                 <i class="icon-info g-pos-rel g-top-1 g-color-gray-dark-v5 g-mr-5"></i><span style="color:green;">PAID</span> 
@@ -42,4 +37,8 @@ function displayUi (country) {
     `
     // did this so i would avoid the process of having a refrsh/update button
     document.getElementById('template').innerHTML = template
+    console.log('Done')
 }
+
+
+fetchInfo ()
---
+++ assets/api2.js
@@ -0,0 +1,22 @@
+function fetchdata(){
+    fetch("https://mawaqif.herokuapp.com/api/checks")
+    .then(response => {
+        if(!response.ok){
+            throw Error("ERROR");
+        }
+        return response.json();
+    })
+    .then(data =>{
+        console.log(data.data);
+        const html = data.data.map(user =>{
+            return `<div class="col-lg-4 g-mb-30"><article class="g-pa-25 u-shadow-v11 rounded"><h2 class="h4 g-mb-15"><a class="u-link-v5 g-color-gray-dark-v1 g-color-primary--hover" href="#">Parking Space ${user.packing_space_id}</a></h2><ul class="list-inline d-flex justify-content-between g-mb-20"><li class="list-inline-item"><i class="icon-eye g-pos-rel g-top-1 g-color-gray-dark-v5 g-mr-5"></i> ${user.plate_number}</li><li class="list-inline-item"><i class="icon-info g-pos-rel g-top-1 g-color-gray-dark-v5 g-mr-5"></i><span style="color:green;">${user.is_empty}</span> </li></ul><p class="g-mb-15"><i class="icon-clock g-pos-rel g-top-1 g-color-gray-dark-v5 g-mr-5"></i> ${user.created_at}</p></article></div>`
+        })
+            .join("");
+        console.log(html);
+        document.querySelector("#app_template").insertAdjacentHTML("afterbegin", html);})
+        .catch(error =>{
+            console.log(error);
+        });
+}
+
+fetchdata();
